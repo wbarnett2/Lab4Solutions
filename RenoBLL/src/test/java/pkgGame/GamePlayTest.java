f9a9db08b6d01df8fb35682b5516ab8bf75d06ee
CISC181 <bgibbons@udel.edu>
2021-04-10 15:29:49 -0400

    Update GamePlayTest.java

--- RenoBLL/src/test/java/pkgGame/GamePlayTest.java
+++ RenoBLL/src/test/java/pkgGame/GamePlayTest.java
@@ -37,26 +37,27 @@ public class GamePlayTest {
 		Rule rle = new Rule(eGame.TexasHoldEm);
 		GamePlay gp = new GamePlay(t, rle);
 		
-		ArrayList<Card> cards = new ArrayList<Card>();
-		cards.add(new Card(eSuit.HEARTS, eRank.TWO));
-		cards.add(new Card(eSuit.HEARTS, eRank.THREE));
+		ArrayList<Card> p1Cards = new ArrayList<Card>();
+		p1Cards.add(new Card(eSuit.HEARTS, eRank.TWO));
+		p1Cards.add(new Card(eSuit.HEARTS, eRank.THREE));
 		
 		Player p1 = new Player("Bert");
 		Player p2 = new Player("Joe");
 		
 		ArrayList<Card> p2Cards = new ArrayList<Card>();
-		cards.add(new Card(eSuit.HEARTS, eRank.ACE));
-		cards.add(new Card(eSuit.DIAMONDS, eRank.ACE));
+		p2Cards.add(new Card(eSuit.HEARTS, eRank.ACE));
+		p2Cards.add(new Card(eSuit.DIAMONDS, eRank.ACE));
 		
 		ArrayList<Card> commonCards = new ArrayList<Card>();
 		commonCards.add(new Card(eSuit.HEARTS, eRank.THREE));
 		commonCards.add(new Card(eSuit.HEARTS, eRank.FOUR));
 		commonCards.add(new Card(eSuit.HEARTS, eRank.FIVE));
+
 		
 		gp = GamePlayHelper.setCommonCards(gp,  commonCards);
 		
-		HandPoker hp1 = HandPokerHelper.SetHand(cards, new HandPoker());
-		HandPoker hp2 = HandPokerHelper.SetHand(p2Cards, new HandPoker());
+		HandPoker hp1 = HandPokerHelper.SetHand(p1Cards, new HandPoker(p1,gp));
+		HandPoker hp2 = HandPokerHelper.SetHand(p2Cards, new HandPoker(p2,gp));
 		
 		gp = GamePlayHelper.PutGamePlay(gp, p1.getPlayerID(), hp1);		
 		gp = GamePlayHelper.PutGamePlay(gp, p2.getPlayerID(), hp2);
@@ -70,3 +71,4 @@ public class GamePlayTest {
 	}
 
 }
+
