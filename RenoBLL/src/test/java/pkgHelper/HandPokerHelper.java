cd001d774b03ef0ab1e1ed5301eb443dd301e815
CISC181 <bgibbons@udel.edu>
2021-04-10 15:30:11 -0400

    Update HandPokerHelper.java

--- RenoBLL/src/test/java/pkgHelper/HandPokerHelper.java
+++ RenoBLL/src/test/java/pkgHelper/HandPokerHelper.java
@@ -12,18 +12,15 @@ import pkgCore.HandPoker;
 public class HandPokerHelper {
 	
 	public static HandPoker SetHand(ArrayList<Card> setCards, HandPoker handpoker) {
-		Object t = null;
 		try {
-			// Load the Class into 'c'
 			Class<?> c = Class.forName("pkgCore.HandPoker");
-			// Create a new instance 't' from the no-arg Deck constructor
-			t = c.newInstance();
+			
 			// Load 'msetCardsInHand' with the 'Draw' method (no args);
 			Method msetCardsInHand = c.getDeclaredMethod("setCards", new Class[] { ArrayList.class });
 			// Change the visibility of 'setCardsInHand' to true *Good Grief!*
 			msetCardsInHand.setAccessible(true);
 			// invoke 'msetCardsInHand'
-			Object oDraw = msetCardsInHand.invoke(t, setCards);
+			Object oDraw = msetCardsInHand.invoke(handpoker, setCards);
 
 		} catch (ClassNotFoundException x) {
 			fail ("Class Not Found Exception Thrown");
@@ -31,8 +28,6 @@ public class HandPokerHelper {
 			fail ("Security Exception Thrown");
 		} catch (IllegalArgumentException e) {
 			fail ("Illegal Arugment Exception Thrown");
-		} catch (InstantiationException e) {
-			fail ("Instantiation Excpetion Thrown");
 		} catch (IllegalAccessException e) {
 			fail ("Illegal Access Exception Thrown");
 		} catch (NoSuchMethodException e) {
@@ -40,7 +35,6 @@ public class HandPokerHelper {
 		} catch (InvocationTargetException e) {
 			fail ("Invocation Target Exception Thrown");
 		}
-		handpoker = (HandPoker) t;
 		return handpoker;
 	}
 }
